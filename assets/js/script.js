17ccdb532bf992e22a85a65fe8a3020e06be3950
jonistyle2023 <jonathanpan22@gmail.com>
2025-05-11 00:55:51 -0500

    ADD particles

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,4 +31,69 @@ $(document).ready(function(){
 
     });
 
+    //cursor
+    $(function(){
+        var body =  document.querySelector('body');
+        var $cursor = $('.cursor');
+        var $cursortwo = $('.cursor-two')
+        function cursormover(e){
+
+            gsap.to( $cursor , {
+                x : e.clientX ,
+                y : e.clientY,
+            })
+            gsap.to( $cursortwo , {
+                x : e.clientX ,
+                y : e.clientY,
+            })
+        }
+        function cursorhover(e){
+            gsap.to( $cursor,{
+                scale:1.5,
+                opacity:.4,
+                background:'rgb(235,235,235)',
+                border:'none',
+                ease: Expo.easeOut,
+            })
+            gsap.to( $cursortwo,{
+                scale:0,
+                opacity:0
+            })
+        }
+        function linkhover(e){
+            gsap.to( $cursor,{
+                scale:1.5,
+                opacity:1,
+                background:'rgb(245,245,245)',
+                border:'none',
+                innerHTML:'view <br> project'
+            })
+            gsap.to( $cursortwo,{
+                scale:0,
+                opacity:0
+            })
+        }
+        function cursor(e){
+            gsap.to( $cursor, {
+                scale:1,
+                opacity:1,
+                background:'transparent',
+                border:'1px solid #3abb94',
+                innerHTML:''
+            })
+            gsap.to( $cursortwo,{
+                scale:1,
+                opacity:1
+            })
+        }
+        $(window).on('mousemove',cursormover);
+        $('a').hover(cursorhover,cursor);
+        $('.control').hover(cursorhover,cursor);
+        $('.hover').hover(cursorhover,cursor);
+        $('.project-link').hover(linkhover,cursor)
+
+    })
+
+    //cursor over
+
 });
